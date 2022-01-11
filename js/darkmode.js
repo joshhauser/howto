4542636f623410914e46cace006ebb56dbaade40
joshhauser <joshua.hauser@gmx.fr>
2022-01-11 15:03:42 +0100

    added darkmod and fixed search

--- js/darkmode.js
+++ js/darkmode.js
@@ -14,8 +14,10 @@ function toggleDarkMode(html) {
   html.classList.toggle("dark");
   let stylesheet = document.getElementById('codeStylesheet');
 
-  if (stylesheet.getAttribute('href') === '../css/highlightjs/stackoverflow-dark.min.css')
-    stylesheet.setAttribute('href', '../css/highlightjs/stackoverflow-light.min.css');
-  else
-    stylesheet.setAttribute('href', '../css/highlightjs/stackoverflow-dark.min.css');
+  if (stylesheet != null) {
+    if (stylesheet.getAttribute('href') === '../css/highlightjs/stackoverflow-dark.min.css')
+      stylesheet.setAttribute('href', '../css/highlightjs/stackoverflow-light.min.css');
+    else
+      stylesheet.setAttribute('href', '../css/highlightjs/stackoverflow-dark.min.css');
+  }
 }
